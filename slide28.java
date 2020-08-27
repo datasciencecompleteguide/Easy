7443a7b6aa6d4feaa0eaa314f1c1665e83f16bfd
Rajeev Nayan Agrawal <69833825+datasciencecompleteguide@users.noreply.github.com>
2020-08-27 13:32:59 +0530

    Solutions

---
+++ slide28.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int repeatedNTimes(int[] A) {
+        for (int k = 1; k <= 3; ++k)
+            for (int i = 0; i < A.length - k; ++i)
+                if (A[i] == A[i+k])
+                    return A[i];
+
+        throw null;
+	}
+	public static void main(String[] args){
+		Solution  obj = new Solution();
+		int arr[] = {1,2,3,3};
+		System.out.println(obj.repeatedNTimes(arr));
+	}
+}
